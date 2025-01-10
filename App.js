7841f68eaabba91ddff3d00a28db68240155e942
Martin Sax <h.martin.sax@gmail.com>
2025-01-10 14:58:23 +0100

    numbers game initial commit

--- App.js
+++ App.js
@@ -1,80 +1,18 @@
-import { useState } from "react";
-import { Button, FlatList, StyleSheet, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
+import { StyleSheet, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function modalVisibilityHandler() {
-    if (!modalIsVisible) setModalIsVisible(true);
-    else setModalIsVisible(false);
-  }
-
-  function addGoalHandler(enteredGoalText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      {
-        text: enteredGoalText,
-        id: enteredGoalText + Math.random().toString(),
-      },
-    ]);
-    modalVisibilityHandler();
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
   return (
-    <>
-      <StatusBar style="light" />
-      <View style={styles.appContainer}>
-        <Button
-          title="lägg till nytt mål"
-          color="#b180f0"
-          onPress={modalVisibilityHandler}
-        />
-        <GoalInput
-          visible={modalIsVisible}
-          onAddGoal={addGoalHandler}
-          onClose={modalVisibilityHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  text={itemData.item.text}
-                  id={itemData.item.id}
-                  onDeleteGoal={deleteGoalHandler}
-                />
-              );
-            }}
-            keyExtractor={(item) => {
-              return item.id;
-            }}
-            alwaysBounceVertical={false}
-          />
-        </View>
-      </View>
-    </>
+    <LinearGradient colors={["#5c0834", "#ddb52f"]} style={styles.rootScreen}>
+      <StartGameScreen />)
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
+  rootScreen: {
     flex: 1,
-    paddingTop: 80,
-    paddingHorizontal: 16,
-  },
-
-  goalsContainer: {
-    flex: 5,
   },
 });
---
+++ screens/StartGameScreen.js
@@ -0,0 +1,61 @@
+import { TextInput, View, StyleSheet } from "react-native";
+
+import PrimaryButton from "../components/PrimaryButton";
+
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={styles.buttonsContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 100,
+    marginHorizontal: 24,
+    backgroundColor: "#57052e",
+    borderRadius: 8,
+    elevation: 4, //android boxshadow
+    shadowColor: "black", //ios boxshadow
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 6,
+  },
+  numberInput: {
+    height: 60,
+    width: 50,
+    fontSize: 36,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttonsContainer: {
+    flexDirection: "row",
+  },
+  buttonContainer: {
+    flex: 1,
+  },
+});
