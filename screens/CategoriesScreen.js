5546f85ab6304dc8832d39a1256fed07919125de
Martin Sax <h.martin.sax@gmail.com>
2025-01-14 13:52:06 +0100

    meals app update

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,20 @@
+import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from "react-native";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem({ item }) {
+  return <CategoryGridTile title={item.title} color={item.color} />;
+}
+
+function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
+  );
+}
+
+export default CategoriesScreen;
